9b0a85ee52e4bdc0e926bde415e05924ae9134d2
ericmiller <emiller@skillstorm.com>
2021-10-20 14:15:54 -0500

    Added Entity Framework nuget packages
    More setup work. Will branch from here.

--- Net-Project-2.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Net_Project_2.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ Net-Project-2.Data/Project2Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Net_Project_2.Data
+{
+    public class Project2Context : DbContext
+    {
+        // DbSets for our classes go here
+
+        public Project2Context(DbContextOptions options) : base(options) { }
+    }
+}
